ed6564af9c56d0fa66327a712fa5a64d598220a8
Steinburglar <159649480+Steinburglar@users.noreply.github.com>
2024-04-05 13:21:48 -0400

    fixed vizualizer!

--- test/metroplois2.py
+++ test/metroplois2.py
@@ -12,7 +12,7 @@ import numba
 import random
 import matplotlib.animation as animation
 import pandas as pd
-
+from IPython.display import Video
 
 class Metroplois:
   
@@ -37,7 +37,7 @@ class Metroplois:
             return False
     
     #@numba.njit("UniTuple(f8[:], 2)(f8[:,:], i8, f8, f8)", nopython=True, nogil=True)
-    def run_metroplois(self, steps):
+    def run_metroplois(self, steps, path, display=False):
         graph_snapshots_array = []
         for i in range(0, steps):
             node = self.random_node()
@@ -45,10 +45,11 @@ class Metroplois:
             if check == False:
                 self.graph.flip(node) #updates graph back to old lattice configuration
             graph_snapshots_array.append(self.graph.lat.copy())
-        self.visualizer(graph_snapshots_array)
+        if display:
+            self.visualizer(graph_snapshots_array, self.T, path,)
             
     
-    def visualizer(self, graph_snapshots_array):
+    def visualizer(self, graph_snapshots_array, T, path):
         fps = 30
         nSeconds = 5
         snapshots = graph_snapshots_array
@@ -57,13 +58,14 @@ class Metroplois:
         fig = plt.figure( figsize=(8,8) )
 
         a = snapshots[0]
-        im = plt.imshow(a, interpolation='none', aspect='auto', vmin=0, vmax=1)
+        im = plt.imshow(a, interpolation='none', aspect='auto', vmin=0, vmax=1,)
         #helper function
         def animate_func(i):
             if i % fps == 0:
                 print( '.', end ='' )
 
             im.set_array(snapshots[i])
+            plt.title('Temperature : '+ str(T))
             return [im]
 
         anim = animation.FuncAnimation(
@@ -73,11 +75,12 @@ class Metroplois:
                                        interval = 1000 / fps, # in ms
                                        )
 
-        anim.save('test_anim.mp4', fps=fps, extra_args=['-vcodec', 'libx264'])
+        anim.save(path, fps=fps, extra_args=['-vcodec', 'libx264'])
+        plt.close()
             
 
                     
-    def plot_mag_temp(self, steps, temp_range):
+    def plot_mag_temp(self, steps, temp_range, temp_step=1):
         """
         method to run a metropolis algorithm for a variety of temperatures and plot
         the final magnetism as a function of that temperature
@@ -85,19 +88,24 @@ class Metroplois:
         @param: temp_range - list- list of the range of temeratures to be sampled,
         should be [lowest, highest, step]
         """
+        path = "Metropolis_anim.mp4"
         mag_array = []  # List to store magnetization values
         temp = []  # List to store temperatures
-        for T in range(1, temp_range, 1):
+        for T in range(1, temp_range, temp_step):
             self.graph.randomize_all()
             self.T = T
-            self.run_metroplois(steps)
+            if T == 5:
+                self.run_metroplois(steps, path, display =True)
+            else:
+                self.run_metroplois(steps, path)
             temp.append(self.T)  # range of temperatures
             mag_array.append(self.graph.getM())  #append sum of spins
 
-        plt.plot(temp, mag_array)
-        plt.xlabel('Temperature')
-        plt.ylabel('Magnetism')
-        plt.grid()
+        fig, ax = plt.subplots(figsize=(12,4))
+        ax.plot(temp, mag_array)
+        ax.set_xlabel('Temperature')
+        ax.set_ylabel('Magnetism')
+        ax.grid()
         plt.show()
     
     """def plot_mag(self, steps, temp_range):
